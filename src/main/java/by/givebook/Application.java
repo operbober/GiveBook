d7bf2a8955d8fd27039e127edc207b3fd4274629
Ivan Malets <operbober@mail.ru>
2015-10-07 13:29:00 +0800

    add maven

---
+++ src/main/java/by/givebook/Application.java
@@ -0,0 +1,19 @@
+package by.givebook;
+
+/**
+ * Created by operb_000 on 07.10.2015.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan
+@EnableJpaRepositories(basePackages = {"habraspring"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
