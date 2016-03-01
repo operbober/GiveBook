c9d8115609aa7901bbfc5363bffd3d0c26090013
Ivan Malets <operbober@mail.ru>
2016-03-01 15:29:18 +0300

    #8, add SimpleServices

--- backendRest/src/main/java/by/givebook/entities/superEntity/enumEntity.java
+++
@@ -1,7 +0,0 @@
-package by.givebook.entities.superEntity;
-
-/**
- * Created by operb_000 on 27.02.2016.
- */
-public class enumEntity {
-}
---
+++ backendRest/src/main/java/by/givebook/services/impl/superServices/SimpleServiceImpl.java
@@ -0,0 +1,30 @@
+package by.givebook.services.impl.superServices;
+
+import by.givebook.services.superServices.SimpleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by operb_000 on 29.02.2016.
+ */
+public class SimpleServiceImpl<T, ID extends Serializable> implements SimpleService<T,ID> {
+    @Autowired
+    CrudRepository<T,ID> entityes;
+
+
+    @Override
+    public T get(ID id) {
+        return entityes.findOne(id);
+    }
+
+    @Override
+    public List<T> getAll() {
+        List<T> allentityes = new ArrayList<>();
+        entityes.findAll().forEach(allentityes::add);
+        return allentityes;
+    }
+}
