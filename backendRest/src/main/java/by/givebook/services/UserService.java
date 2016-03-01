462908370ace6b595984cb39b9617b1792afc074
Ivan Malets <operbober@mail.ru>
2016-03-01 15:34:25 +0300

    #9, add author controller and service, add user service

---
+++ backendRest/src/main/java/by/givebook/services/UserService.java
@@ -0,0 +1,9 @@
+package by.givebook.services;
+
+import by.givebook.entities.User;
+import by.givebook.services.superServices.SimpleService;
+
+/**
+ * Created by operb_000 on 29.02.2016.
+ */
+public interface UserService extends SimpleService<User, Long> {}
