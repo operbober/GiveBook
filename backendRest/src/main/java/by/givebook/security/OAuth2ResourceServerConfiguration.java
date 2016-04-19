c2af50f7751a57c7ec9d7481649d2b923dfac72d
Ivan Malets <operbober@mail.ru>
2016-04-19 10:49:27 +0300

    #30, add roles to authorization request

--- backendRest/src/main/java/by/givebook/security/OAuth2ResourceServerConfiguration.java
+++ backendRest/src/main/java/by/givebook/security/OAuth2ResourceServerConfiguration.java
@@ -27,7 +27,6 @@ public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerA
                 .antMatchers(HttpMethod.GET, "/offerTypes/").authenticated()
                 .antMatchers(HttpMethod.PUT, "/myOffers/").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/myOffers/").authenticated()
-                .antMatchers(HttpMethod.PUT, "/offers/").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN");
     }
